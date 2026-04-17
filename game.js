82015722e30d66aef825b731bcfe6e274ef11e08
11Cece <celineelbanna74@gmail.com>
2026-04-17 10:44:26 -0700

    :D

---
+++ game.js
@@ -0,0 +1,10 @@
+// health bar
+let health = document.getElementById('health_bar');
+function healthBar () {}
+
+
+// menu
+// let playBtn = document.getElementById('btn');
+// playBtn.addEventListener('click', () => {
+
+// });
