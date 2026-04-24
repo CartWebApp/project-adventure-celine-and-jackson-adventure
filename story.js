94e55b99e0a27c036a03780b68812c14bad07612
11Cece <celineelbanna74@gmail.com>
2026-04-24 09:39:47 -0700

    Story time!

---
+++ story.js
@@ -0,0 +1,10 @@
+const story = {
+    intro: {
+        text: "You wake up in a dark room. You can't remember how you got here. You see a door to your left and a window to your right.",
+        choices: [
+            { text: "Go through the door", next: "door" },
+            { text: "Look out the window", next: "window" }
+        ],
+        background: "dark_room.jpg"   
+    }
+}
