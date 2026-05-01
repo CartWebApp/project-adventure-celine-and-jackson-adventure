2828107cc0af7ee48c5bed2e503f9a79a7f31edf
11Cece <celineelbanna74@gmail.com>
2026-05-01 10:28:47 -0700

    Update story.js
    Not in Paragraphs

--- story.js
+++ story.js
@@ -39,7 +39,7 @@ const story = {
 
     touch: {
 
-        text: ["You touch the stone. Suddenly, you are hit with a bright flash of light.","As the light begins to fade, and you regain your vision, nothing seemed to change. You exit the cave...","'Where am I?'","'This isn't the forest.'","You head back into the cave and grab the stone and try to use it again in hopes that it would bring you back.","Nothing happens, but then a voice resonates from the stone.","'All charges used, catalyst empty, now self destructing in 10 seconds.'","Boulder or Tree?"], 
+        text: ["You touch the stone. Suddenly, you are hit with a bright flash of light.As the light begins to fade, and you regain your vision, nothing seemed to change. You exit the cave... 'Where am I?''This isn't the forest.'You head back into the cave and grab the stone and try to use it again in hopes that it would bring you back. Nothing happens, but then a voice resonates from the stone. 'All charges used, catalyst empty, now self destructing in 10 seconds.' Boulder or Tree?"], 
         choices: [
             { text: "Boulder", next: "Boulder" },
             { text: "Tree", next: "Tree", healthChange: -100 },
